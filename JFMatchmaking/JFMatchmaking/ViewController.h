4d964e2999061ea13994888f6b45387a6693a083
杨凡 <799862527@qq.com>
2016-05-17 19:51:46 +0800

    创建工程

---
+++ JFMatchmaking/JFMatchmaking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFMatchmaking
+//
+//  Created by 杨凡 on 16/5/17.
+//  Copyright © 2016年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JFMatchmaking/JFMatchmaking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JFMatchmaking
+//
+//  Created by 杨凡 on 16/5/17.
+//  Copyright © 2016年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
