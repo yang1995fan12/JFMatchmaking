e6f6acc91f8e227d1b7db2e36616d23f724a9eef
杨凡 <799862527@qq.com>
2016-05-20 04:16:11 +0800

    no message

---
+++ JFMatchmaking/JFMatchmaking/User.h
@@ -0,0 +1,20 @@
+//
+//  User.h
+//  JFMatchmaking
+//
+//  Created by 杨凡 on 16/5/19.
+//  Copyright © 2016年 yf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic,strong) NSString *objectId;
+@property (nonatomic,strong) NSString *username;
+
++ (instancetype)currentUser;
++ (void)loginWithID:(NSString *)objectId username:(NSString *)username;
++ (void)logout;
+
+@end
